1c06cfdea074671fc600e820d3bdf6d356fc74a9
Bikram Karki <himalayanepalinsis@gmail.com>
2020-03-29 16:09:31 +0300

    added public and private route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect, Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+
+import { auth } from './services/firebase';
+
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/chat" />
+        )
+      }
+    />
+  );
+}
 function App() {
-  return <div className="App">Hello</div>;
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      let userIsAuthenticated = false;
+
+      if (user) {
+        userIsAuthenticated = true;
+      }
+
+      setAuthenticated(userIsAuthenticated);
+      setLoading(false);
+    });
+  });
+
+  if (loading) return <h2>Loading...</h2>;
+
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <PrivateRoute
+            path="/chat"
+            authenticated={authenticated}
+            component={Chat}
+          ></PrivateRoute>
+          <PublicRoute
+            path="/signup"
+            authenticated={authenticated}
+            component={Signup}
+          ></PublicRoute>
+          <PublicRoute
+            path="/login"
+            authenticated={authenticated}
+            component={Login}
+          ></PublicRoute>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
